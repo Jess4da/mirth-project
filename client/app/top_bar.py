e022557d41c18bbe0c5f005b7dc54f984eed9463
Jessada <jessadad99@gmail.com>
2022-03-16 14:08:01 +0700

    test01

--- client/app/top_bar.py
+++ client/app/top_bar.py
@@ -8,7 +8,7 @@ class TopBar(object):
     def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
-        self.width = width 
+        self.width = width
         self.height = height
         self.word = ""
         self.round = 1
@@ -18,35 +18,46 @@ class TopBar(object):
         self.BORDER_THICKNESS = 5
         self.time = 60
         self.drawing = False
+        self.top_vowel = ['ิ', '้']
+        self.bottom_vowel = ['ุ']
 
     def draw(self, win):
-        pygame.draw.rect(win, (255,255,255), (self.x ,self.y, self.width, self.height), self.BORDER_THICKNESS)
+        pygame.draw.rect(win, (255, 255, 255), (self.x, self.y, self.width, self.height), self.BORDER_THICKNESS)
 
         # draw round
-        txt = self.round_font.render(f"Round {self.round} of {self.max_round}", 1, (255,255,255))
+        txt = self.round_font.render(f"Round {self.round} of {self.max_round}", 1, (255, 255, 255))
         win.blit(txt, (self.x + 10, self.y + self.height/2 - txt.get_height()/2))
+
+        self.word = 'หิว'
 
         # draw underscores
         if self.drawing:
             wrd = self.word
         else:
-            wrd = TopBar.underscore_text(self.word)
-        txt = self.word_font.render(wrd, 1, (255,255,255))
-        win.blit(txt, (self.x + self.width/2 - txt.get_width()/2, self.y + self.height/2 - txt.get_height()/2 + 10))
+            wrd = self.underscore_text(self.word)
 
-        pygame.draw.circle(win, (255,255,255), (self.x + self.width - 50, self.y + round(self.height/2)), 40,0)
-        timer = self.round_font.render(str(self.time), 1, (0,0,0))
+        txt = self.word_font.render(wrd, 1, (255, 255, 255))
+        if self.drawing:
+            win.blit(txt, (self.x + self.width/2 - txt.get_width()/2, self.y + self.height/2 - txt.get_height()/2 + 10))
+        else:
+            for i, n in enumerate(wrd.split('_')):
+                temp_txt = self.word_font.render(n, 1, (255, 255, 255))
+                win.blit(temp_txt, (self.x + self.width/2 - txt.get_width()/2 + (i*50), self.y + self.height/2 - txt.get_height()/2 + 10))
+
+        pygame.draw.circle(win, (255, 255, 255), (self.x + self.width - 50, self.y + round(self.height/2)), 40, 0)
+        timer = self.round_font.render(str(self.time), 1, (0, 0, 0))
         win.blit(timer, (self.x + self.width - 50 - timer.get_width()/2, self.y + self.height/2 - timer.get_height()/2))
 
-    @staticmethod
-    def underscore_text(text):
+    def underscore_text(self, text):
         new_str = ""
 
+        accept_char = ['-', ' ']
+
         for char in text:
-            if char != " ":
-                new_str += " _ "
+            if char not in accept_char:
+                new_str += " _ " if char not in self.top_vowel + self.bottom_vowel else char
             else:
-                new_str += "   "
+                new_str += f" {char} "
 
         return new_str
 
